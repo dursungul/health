21f0d54a5598dcdc40ef02ae06f59cf4d3993ee0
dursungul <dursundiy@gmail.com>
2022-04-03 02:20:42 +0300

    3mart saat 02.00 ilerleme

--- lib/pages/vki_page.dart
+++ lib/pages/vki_page.dart
@@ -12,12 +12,41 @@ class VkiHesapla extends StatefulWidget {
 class _VkiHesaplaState extends State<VkiHesapla> {
   TextEditingController kiloController = new TextEditingController();
   TextEditingController boyController = new TextEditingController();
+  TextEditingController yasController = new TextEditingController();
+  TextEditingController cinsController = new TextEditingController();
+
+  int sayi = 5;
+  double kilo = 0;
+  double boy = 0;
+  double vki = 0;
+  int yas = 0;
+  String cins = "";
+  int kiloclass = 0;
+
+  void Hesapla() {
+    setState(() {
+      kilo = double.parse(kiloController.value.text);
+      boy = double.parse(boyController.value.text);
+      yas = int.parse(yasController.value.text);
+      cins = cinsController.value.text;
+      vki = Kiloindexhesapla(boy, kilo);
+      kiloclass = KiloClassBul(vki);
+      print("kilo: " + kilo.toString());
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Text("Profile"),
+        Row(
+          //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            inputAlan("YAS", yasController),
+            inputAlan("CİNSİYET", cinsController)
+          ],
+        ),
         SizedBox(
           height: 10,
         ),
@@ -39,10 +68,20 @@ class _VkiHesaplaState extends State<VkiHesapla> {
           height: 30,
         ),
         Container(
-          child: Text("Kilo durumu"),
+          child: Text("Kilo durumu : " + "$vki"),
           color: Colors.white,
         ),
+        ElevatedButton(
+          onPressed: () {
+            Hesapla();
+          },
+          child: Text('Looks like a RaisedButton'),
+        )
       ],
     );
   }
+}
+
+int Numberop(int num) {
+  return num * 2;
 }
